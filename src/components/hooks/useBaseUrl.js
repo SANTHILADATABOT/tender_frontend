0cec2e9ca517f751668f1b01abfca1ba836e1933
prabakaran72 <prabakaranpbk@gmail.com>
2023-01-07 22:24:21 +0530

    Test Entry

--- src/components/hooks/useBaseUrl.js
+++ src/components/hooks/useBaseUrl.js
@@ -1,8 +1,8 @@
 const useBaseUrl = () => {
-    return {
-        server1 : "http://192.168.1.29:8000",
-        server2 : "http://localhost:8000"
-    }
-}
+  return {
+    server1: "http://192.168.1.36:8000",
+    server3: "http://localhost:8000",
+  };
+};
 
-export {useBaseUrl};
+export { useBaseUrl };
