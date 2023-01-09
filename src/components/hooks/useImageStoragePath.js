b10b158e0476ded8511448e83cec36c66b65a4ff
USER <prabakaranpbk@gmail.com>
2023-01-09 16:05:14 +0530

    Live checked

--- src/components/hooks/useImageStoragePath.js
+++ src/components/hooks/useImageStoragePath.js
@@ -3,9 +3,9 @@ import { useBaseUrl } from "./useBaseUrl";
 const useImageStoragePath = () => {
     const { server1: baseUrl } = useBaseUrl();  
     return {
-        qcFile : baseUrl+'/uploads/competitor/qc/',
-        woFile : baseUrl+'/uploads/competitor/woFile/',
-        woCompletionFile : baseUrl+'/uploads/competitor/woCompletionFile/',
+        qcFile : baseUrl+'/public/uploads/competitor/qc/',
+        woFile : baseUrl+'/public/uploads/competitor/woFile/',
+        woCompletionFile : baseUrl+'/public/uploads/competitor/woCompletionFile/',
                
     }
 }
